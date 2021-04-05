3d9ed786bcd7ba3afbcaef43cd0ee47f7de3b5b7
NathanLouth <nathanlouth@outlook.com>
2021-04-05 16:14:27 +0100

    Main.cpp add Code

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
 using namespace std;
 
+bool prime(unsigned long long int x){
+
+    for(unsigned long long int i = (x-1);i > 1;i--){
+        if(x % i == 0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main(){
+
+    unsigned long long int num;
+    cout << "Please enter a number and this program will display all the prime number below it:" << endl;
+    cin >> num;
+
+    for(unsigned long long int x = num;x > 1;x--){
+        if(prime(x)){
+            cout << x << endl;
+        }
+    }
 
     return 0;
 }
